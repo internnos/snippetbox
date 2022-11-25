91c3c860759d391cb582bce0cf14e034a898b2f5
internos <amajidsinar@gmail.com>
2022-11-25 17:27:37 +0700

    fix(snippetCreate): add missing return

--- main.go
+++ main.go
@@ -19,11 +19,13 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
+		w.Header().Set("Allow", "POST")
 		// caveat:
 		// WriteHeader can only be written once!
 		// if WriteHeader is not put before Write, then the status would still be 200!
 		w.WriteHeader(405)
 		w.Write([]byte("Method Not Allowed"))
+		return
 	}
 	w.Write([]byte("Create a specific snippet ..."))
 }
