f9f44946e086498f911bc04a9b0490893edcd4b0
internos <amajidsinar@gmail.com>
2022-11-25 17:22:22 +0700

    feat(snippetCreate): restrict to only post

--- main.go
+++ main.go
@@ -6,6 +6,10 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 	w.Write([]byte("Hello from snippetbox"))
 }
 
@@ -14,6 +18,13 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		// caveat:
+		// WriteHeader can only be written once!
+		// if WriteHeader is not put before Write, then the status would still be 200!
+		w.WriteHeader(405)
+		w.Write([]byte("Method Not Allowed"))
+	}
 	w.Write([]byte("Create a specific snippet ..."))
 }
 
